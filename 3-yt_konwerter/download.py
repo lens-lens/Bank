a2a979d922b6a3f1a26e8e1103933e46b39032dd
Milena Napierała <m.napierala24@gmail.com>
2023-06-07 22:01:27 +0200

    download stream

---
+++ 3-yt_konwerter/download.py
@@ -0,0 +1,17 @@
+import argparse
+from pytube import YouTube
+
+def Download(link):
+    yt_object = YouTube(link)
+    yt_object = yt_object.streams.get_highest_resolution.()
+
+    try:
+        yt_object.download()
+    except:
+        print('An error has occured')
+    print('Download is completed succesfully')
+
+link = input('Enter the YouTube video URL')
+
+go = Download(link)
+    # return go(f, seed, [])
